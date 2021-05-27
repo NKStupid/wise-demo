6758545aa43feb074d4b0fb78331a6e123042128
NKStupid <64253633+NKStupid@users.noreply.github.com>
2021-05-27 21:09:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,24 +6,26 @@ const fs = require("fs");
 const express = require("express");
  const app = express();
 //
- app.get("/", (req, res) => res.send(`Hello World!`));
- https
- .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", resp => {
-             let data = "";
+ app.get("/", (req, res) => {
+  res.send(`Hello World!`);
+  https
+  .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", resp => {
+              let data = "";
 
-             resp.on("data", chunk => {
-                           data += chunk;
-                         });
+              resp.on("data", chunk => {
+                            data += chunk;
+                          });
 
-             resp.on("end", () => {
-                           let url = JSON.parse(data).hdurl;
-                           console.log(url);
+              resp.on("end", () => {
+                            let url = JSON.parse(data).hdurl;
+                            console.log(url);
 
-                         });
-           })
- .on("error", err => {
-             console.log("Error: " + err.message);
-           });
+                          });
+            })
+  .on("error", err => {
+              console.log("Error: " + err.message);
+            });
+  });
 //
  app.listen(3000, () => {
    console.log(`Example app listening on port 3000!`);
