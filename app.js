ff3a905e19951ae2d213adc1c8439ec307a8bb19
NKStupid <64253633+NKStupid@users.noreply.github.com>
2021-05-27 21:06:38 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+// app.js
+const https = require("https");
+
+const Stream = require("stream").Transform;
+const fs = require("fs");
+const express = require("express");
+ const app = express();
+//
+ app.get("/", (req, res) => res.send(`Hello World!`));
+ https
+ .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", resp => {
+             let data = "";
+
+             resp.on("data", chunk => {
+                           data += chunk;
+                         });
+
+             resp.on("end", () => {
+                           let url = JSON.parse(data).hdurl;
+                           console.log(url);
+
+                         });
+           })
+ .on("error", err => {
+             console.log("Error: " + err.message);
+           });
+//
+ app.listen(3000, () => {
+   console.log(`Example app listening on port 3000!`);
+
+   });
