91cffd138cc294fe15578781b21e75e27a20fc20
NKStupid <64253633+NKStupid@users.noreply.github.com>
2022-01-18 20:18:17 +0800

    Update testPrivateLink.sh

--- testPrivateLink.sh
+++ testPrivateLink.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-# This is a comment!
-echo Hello World	# This is a comment, too!
+
+echo Hello World	
+echo ----------Begin curl through Private Link----------
+
 curl http://vpce-0f9b25165e4882a9f-wtxlncsb-cn-northwest-1b.vpce-svc-0af8167c9dcc695ad.cn-northwest-1.vpce.amazonaws.com.cn:8080
+
+echo ----------End curl through Private Link----------
