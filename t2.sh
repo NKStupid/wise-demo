31d3b7523a188d21903f47f9c5641eb06ea63a4e
NKStupid <64253633+NKStupid@users.noreply.github.com>
2022-01-18 21:19:17 +0800

    Update t2.sh

--- t2.sh
+++ t2.sh
@@ -3,6 +3,7 @@
 apt install -y wget
 echo Hello World	
 echo ----------Begin curl through Private Link----------
+wget https://www.google.com
 echo ----------End curl through Private Link----------
 
 nginx -g 'daemon off;'
