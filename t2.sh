50b15b9be4ed70376acab1e171f15e69c5ddda63
NKStupid <64253633+NKStupid@users.noreply.github.com>
2022-01-18 22:38:01 +0800

    Update t2.sh

--- t2.sh
+++ t2.sh
@@ -2,7 +2,7 @@
 
 echo Hello World	
 echo ----------Begin curl through Private Link----------
-curl https://www.baidu.com
+curl https://www.baidu.com --output baidu.txt
 echo ----------End curl through Private Link----------
 
 nginx -g 'daemon off;'
