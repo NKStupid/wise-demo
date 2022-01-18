fef9f3896c7d5837c6e0ebf974775cd0a787644d
NKStupid <64253633+NKStupid@users.noreply.github.com>
2022-01-18 20:07:04 +0800

    Update testPrivateLink.sh

--- testPrivateLink.sh
+++ testPrivateLink.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+# This is a comment!
+echo Hello World	# This is a comment, too!
 curl http://vpce-0f9b25165e4882a9f-wtxlncsb-cn-northwest-1b.vpce-svc-0af8167c9dcc695ad.cn-northwest-1.vpce.amazonaws.com.cn:8080
