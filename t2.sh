0da7ff77d7a66234ea555307ccd1fb9916991012
NKStupid <64253633+NKStupid@users.noreply.github.com>
2022-01-18 21:09:53 +0800

    Update t2.sh

--- t2.sh
+++ t2.sh
@@ -2,7 +2,7 @@
 
 echo Hello World	
 echo ----------Begin curl through Private Link----------
-curl --header www.google.com
+curl https://www.google.com
 echo ----------End curl through Private Link----------
 
 nginx -g 'daemon off;'
