c738cf86ef4e87b91d699380c3a0baf650dc3823
NKStupid <64253633+NKStupid@users.noreply.github.com>
2022-01-18 20:42:08 +0800

    Update testPrivateLink.sh

--- testPrivateLink.sh
+++ testPrivateLink.sh
@@ -3,8 +3,7 @@
 echo Hello World	
 echo ----------Begin curl through Private Link----------
 
-curl --header http://vpce-0f9b25165e4882a9f-wtxlncsb-cn-northwest-1b.vpce-svc-0af8167c9dcc695ad.cn-northwest-1.vpce.amazonaws.com.cn:8080/order_mgmt/internal_events/v1/ce_im_dummy_request_1a55dded-2740-494e-a650-
-f34dc06a8282
+curl --header http://vpce-0f9b25165e4882a9f-wtxlncsb-cn-northwest-1b.vpce-svc-0af8167c9dcc695ad.cn-northwest-1.vpce.amazonaws.com.cn:8080/order_mgmt/internal_events/v1/ce_im_dummy_request_1a55dded-2740-494e-a650-f34dc06a8282
 
 # telnet vpce-0f9b25165e4882a9f-wtxlncsb-cn-northwest-1b.vpce-svc-0af8167c9dcc695ad.cn-northwest-1.vpce.amazonaws.com.cn 8080
 
